7eb9023f67c4419efa4ea2c85c369c42f558704f
stkossman <an.stawski@outlook.com>
2026-01-16 02:21:33 +0200

    feat: add hero section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,16 @@
+import HeroSection from '../features/movies/components/HeroSection'
+
 const HomePage = () => {
   return (
-    <div className='container mx-auto px-4 py-10'>
-      <h1 className='text-4xl font-bold tracking-tight text-white mb-4'>
-        Прем'єри тижня
-      </h1>
-      <p className='text-zinc-400'>Тут буде список фільмів...</p>
+    <div className='flex flex-col gap-10 pb-20'>
+      <HeroSection />
+
+      <div className='container mx-auto px-4'>
+        <h2 className='mb-6 text-2xl font-bold text-white'>Зараз у кіно</h2>
+        <div className='grid h-64 w-full place-items-center rounded-xl border border-dashed border-zinc-800 bg-zinc-900/50 text-zinc-500'>
+          Тут буде список фільмів (Grid)
+        </div>
+      </div>
     </div>
   )
 }
