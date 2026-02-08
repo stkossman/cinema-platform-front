f39ffd703837005ad0805b0c46b574059ba6eb3b
stkossman <an.stawski@outlook.com>
2026-02-08 02:10:19 +0200

    feat(MainLayout): add dynamic padding for non-home pages and improve layout responsiveness (#9)

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,14 +1,19 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Header from '../layouts/components/Header'
 import Footer from '../layouts/components/Footer'
 import ScrollToTop from '../common/components/ScrollToTop'
+import { clsx } from 'clsx'
 
 const MainLayout = () => {
+  const location = useLocation()
+  const isHomePage = location.pathname === '/'
   return (
     <div className='flex min-h-screen flex-col bg-[var(--bg-main)] text-[var(--text-main)] antialiased selection:bg-[var(--color-primary)]/30'>
       <ScrollToTop />
       <Header />
-      <main className='flex-1 relative z-0'>
+      <main
+        className={clsx('flex-1 relative z-0', !isHomePage && 'pt-24 md:pt-28')}
+      >
         <Outlet />
       </main>
       <Footer />
